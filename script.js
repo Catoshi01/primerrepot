a870fb3d2f7abc746b7a186d8dc6236426e2cdfe
jonathancastillo <catojota@gmail.com>
2023-10-25 01:39:31 -0600

    mi primer commit de frontend

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript para interacciones en la tienda (puedes personalizar según sea necesario)
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('¡Producto añadido al carrito!');
+        // Aquí puedes agregar la lógica para agregar productos al carrito.
+    });
+});
